db4869a982a6b21e440e1fb70946e70b120688bd
Alex Kirk <alex.git@kirk.at>
2018-04-26 19:04:20 +0200

    Add title to frontend editor

--- templates/friends/editor.php
+++ templates/friends/editor.php
@@ -1,6 +1,7 @@
 <form method="post" action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" class="friends-post-inline">
-	<input type="hidden" name="action" value="friends_publish" />
 	<?php wp_nonce_field( 'friends_publish' ); ?>
+	<input type="hidden" name="action" value="friends_publish" />
+	<input type="text" name="title" value="" placeholder="<?php echo esc_attr( __( 'Title' ) ); ?>" /><br />
 	<textarea name="content" rows="5" cols="70" placeholder="<?php echo esc_attr( sprintf( __( 'What are you up to, %s?', 'friends' ), wp_get_current_user()->display_name ) ); ?>"></textarea><br/>
 	<button>Post to your friends</button>
 	<span style="margin-left: 2em"><input type="hidden" name="status" value="private" /> Published Privately</span>
