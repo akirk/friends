6161c7a6d8019e711e35fb798f0fef9961b0b33c
Alex Kirk <alex.git@kirk.at>
2018-11-06 13:57:15 +0100

    Only show a message if a message exists

--- templates/friends/posts.php
+++ templates/friends/posts.php
@@ -112,18 +112,32 @@ include __DIR__ . '/header.php'; ?>
 					?>
 					<p class="friend-recommendation">
 					<?php
-					echo wp_kses(
-						// translators: %s is the friend's name.
-						sprintf( __( 'Your friend %1$s recommended this with the message: %2$s', 'friends' ), $friend_name, '<span>' . esc_html( $recommendation ) . '</span>' ),
-						array(
-							'a' => array(
-								'class'      => array(),
-								'data-token' => array(),
-								'href'       => array(),
-							),
-						)
-					);
-
+					if ( is_string( $recommendation ) && '1' !== $recommendation ) {
+						echo wp_kses(
+							// translators: %1$s is the friend's name, %2$s is the message.
+							sprintf( __( 'Your friend %1$s recommended this with the message: %2$s', 'friends' ), $friend_name, '<span>' . esc_html( $recommendation ) . '</span>' ),
+							array(
+								'a'    => array(
+									'class'      => array(),
+									'data-token' => array(),
+									'href'       => array(),
+								),
+								'span' => array(),
+							)
+						);
+					} else {
+						echo wp_kses(
+							// translators: %s is the friend's name.
+							sprintf( __( 'Your friend %s recommended this.', 'friends' ), $friend_name ),
+							array(
+								'a' => array(
+									'class'      => array(),
+									'data-token' => array(),
+									'href'       => array(),
+								),
+							)
+						);
+					}
 					echo ' ', esc_html__( 'Be aware that this post might have been altered by your friend. Please verify with the original when in doubt.', 'friends' );
 					?>
 					</p>
