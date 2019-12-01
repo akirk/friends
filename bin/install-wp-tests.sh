92f83330bf09b22cede1ef24dca200dbce704c62
Alex Kirk <alex.git@kirk.at>
2019-12-01 13:42:18 +0100

    Slow down svn co

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -105,6 +105,7 @@ install_test_suite() {
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
 		svn co https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		sleep 5
 		svn co https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
 	fi
 
