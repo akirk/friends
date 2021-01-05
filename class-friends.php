862d4eef8722849848167df563f10da937238838
Alex Kirk <alex.git@kirk.at>
2021-01-05 17:44:45 +0100

    Version bump

--- class-friends.php
+++ class-friends.php
@@ -14,7 +14,7 @@
  * @author Alex Kirk
  */
 class Friends {
-	const VERSION       = '1.0';
+	const VERSION       = '1.01';
 	const CPT           = 'friend_post_cache';
 	const FEED_URL      = 'friends-feed-url';
 	const PLUGIN_URL    = 'https://wordpress.org/plugins/friends/';
--- friends.php
+++ friends.php
@@ -3,7 +3,7 @@
  * Plugin name: Friends
  * Plugin author: Alex Kirk
  * Plugin URI: https://github.com/akirk/friends
- * Version: 1.0
+ * Version: 1.01
  *
  * Description: Decentralized Social Networking with WordPress. Connect WordPresses through friend requests and read each other’s (private) posts in a feed reader.
  *
