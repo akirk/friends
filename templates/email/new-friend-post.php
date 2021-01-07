a5b134c761b0901e3f5477b7c242e506ab451e4a
Alex Kirk <alex.git@kirk.at>
2021-01-07 09:43:37 +0100

    Use replacement title for status posts in e-mail notifications

--- templates/email/new-friend-post.php
+++ templates/email/new-friend-post.php
@@ -9,22 +9,26 @@
 <?php include __DIR__ . '/header.php'; ?>
 <blockquote>
 	<?php
-	echo $post->post_content;
+	echo wp_kses_post( $post->post_content );
 	?>
 </blockquote>
 
 <p>
 	<?php
+	$post_title = $post->post_title;
+	if ( '' === trim( $post_title ) ) {
+		$post_title = $post->post_date;
+	}
 	printf(
 		// translators: %1$s is a username, %2$s is a post title as a link.
 		__( 'This post was published by your friend %1$s: %2$s', 'friends' ),
-		'<em>' . $author->display_name . '</em>',
-		'<strong><a href="' . get_permalink( $post ) . '">' . $post->post_title . '</a></strong>'
+		'<em>' . esc_html( $author->display_name ) . '</em>',
+		'<strong><a href="' . get_permalink( $post ) . '">' . esc_html( $post_title ) . '</a></strong>'
 	);
 	?>
 	<a href="<?php echo site_url( '/friends/' . $post->ID . '/' ); ?>">
 		<?php
-			esc_html_e( 'You can view post on your friends page', 'friends' );
+			esc_html_e( 'View the post on your friends page', 'friends' );
 		?>
 	</a>
 </p>
@@ -33,7 +37,7 @@
 	<?php
 	printf(
 		// translators: %1$s is a URL, %2$s is a URL, %3$s is a username.
-		__( 'Manage your <a href=%1$s>subscription settings</a> or <a href=%2$s>just unsubscribe from %3$s\'s posts</a>', 'friends' ),
+		__( 'Manage your <a href=%1$s>subscription settings</a> or <a href=%2$s>stop notifications for %3$s\'s posts</a>', 'friends' ),
 		'"' . esc_url( self_admin_url( 'admin.php?page=friends-settings' ) ) . '"',
 		'"' . esc_url( self_admin_url( 'admin.php?page=edit-friend&user=' . $author->ID ) ) . '"',
 		'<em>' . $author->display_name . '</em>'
