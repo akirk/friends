6b5c5ea45f142a2b8805d8bf7644ca7e781e20fe
Alex Kirk <akirk@users.noreply.github.com>
2022-10-16 09:43:37 +0200

    Make status view narrower

--- templates/frontend/parts/content-status.php
+++ templates/frontend/parts/content-status.php
@@ -8,7 +8,7 @@
 
 $template_loader = Friends\Friends::template_loader();
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'm-2 p-2 column col-12' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'column col-8 col-sm-12 col-mx-auto' ); ?>>
 	<?php
 	$template_loader->get_template_part( 'frontend/parts/header', get_post_format(), $args );
 	$template_loader->get_template_part( 'frontend/parts/entry-content', get_post_format(), $args );
