9df8fb5f8532c4520f7e3a32cae89e49255572f5
Alex Kirk <akirk@users.noreply.github.com>
2023-03-03 13:35:18 +0100

    Remove stale ActivityPub version and use the information from WordPress.org (#195)

--- templates/admin/plugin-info.php
+++ templates/admin/plugin-info.php
@@ -19,6 +19,12 @@ $more_info_url = $api->more_info;
 
 		<div class="desc column-description">
 			<p><?php echo esc_html( $api->short_description ); ?></p>
+			<?php if ( isset( $api->comment ) ) : ?>
+			<p>
+				<strong><?php esc_html_e( 'Comment from the author of the Friends Plugin', 'friends' ); ?></strong><br/>
+				<?php echo esc_html( $api->comment ); ?>
+			</p>
+			<?php endif; ?>
 			<p class="authors">
 				<cite>
 					<?php
@@ -39,7 +45,7 @@ $more_info_url = $api->more_info;
 	<div class="plugin-card-bottom">
 		<a class="<?php echo esc_attr( $args['button_classes'] ); ?>" data-slug="<?php echo esc_attr( $api->slug ); ?>" data-name="<?php echo esc_attr( $api->name ); ?>" href="<?php echo esc_url( $args['install_url'] ); ?>" aria-label="<?php echo /* translators: %1$s is a plugin name, %2$s is a plugin version. */ esc_html( sprintf( __( 'Install %1$s %2$s now', 'friends' ), $api->name, $api->version ) ); ?>"><?php echo esc_html( $args['button_text'] ); ?></a>
 
-		<a class="button details" href="<?php echo esc_url( $more_info_url ); ?>" aria-label="<?php /* phpcs:ignore WordPress.WP.I18n.MissingArgDomain */ echo /* translators: %s is a plugin name. */ esc_html( sprintf( __( 'More information about %s' ), $api->name ) ); ?>" data-title="<?php echo esc_attr( $api->name ); ?>"><?php esc_html_e( 'More Details' ); ?></a>
+		<a class="button details thickbox open-plugin-details-modal" href="<?php echo esc_url( $more_info_url ); ?>" aria-label="<?php /* phpcs:ignore WordPress.WP.I18n.MissingArgDomain */ echo /* translators: %s is a plugin name. */ esc_html( sprintf( __( 'More information about %s' ), $api->name ) ); ?>" data-title="<?php echo esc_attr( $api->name ); ?>"><?php esc_html_e( 'More Details' ); ?></a>
 
 		<a class="button deactivate <?php echo esc_attr( $args['deactivate_button_class'] ); ?>"
 			data-slug="<?php echo esc_attr( $api->slug ); ?>"
