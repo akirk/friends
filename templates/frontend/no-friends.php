c122a3bf2b75e2443aa06db835b6b0e699e287ae
Alex Kirk <akirk@users.noreply.github.com>
2023-07-22 19:20:23 +0200

    Add note about how to find the welcome text again

--- templates/frontend/no-friends.php
+++ templates/frontend/no-friends.php
@@ -13,3 +13,20 @@ Friends\Friends::template_loader()->get_template_part(
 		'plugin-list' => false,
 	)
 );
+
+?><p class="note">
+	<?php
+	echo wp_kses(
+		sprintf(
+			// translators: %s is the URL of the Friends admin menu.
+			__( '<strong>Note:</strong> This box will go away as soon as you have added your first friend or subscription.<br/>It will remain available via the <a href=%s>Friends admin menu</a> for later reference.', 'friends' ),
+			'"' . admin_url( 'admin.php?page=friends' ) . '"'
+		),
+		array(
+			'a'      => array( 'href' => array() ),
+			'br'     => array(),
+			'strong' => array(),
+		)
+	);
+	?>
+</p>
