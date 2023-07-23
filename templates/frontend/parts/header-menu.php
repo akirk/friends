7374a5e9d12df2cb4d5c6501d4b440b5de9749a4
Alex Kirk <akirk@users.noreply.github.com>
2023-07-23 14:58:47 +0200

    Allow trashing of Post Collection posts (#240)

--- templates/frontend/parts/header-menu.php
+++ templates/frontend/parts/header-menu.php
@@ -31,10 +31,12 @@ if ( apply_filters( 'friends_debug', false ) ) : ?>
 <?php if ( current_user_can( 'edit_post', get_current_user_id(), get_the_ID() ) ) : ?>
 	<li class="menu-item"><?php edit_post_link(); ?></li>
 <?php endif; ?>
-<?php if ( in_array( get_post_type(), apply_filters( 'friends_frontend_post_types', array() ), true ) && apply_filters( 'friends_show_author_edit', true, $args['friend_user'] ) ) : ?>
+<?php if ( in_array( get_post_type(), apply_filters( 'friends_frontend_post_types', array() ), true ) ) : ?>
+	<?php if ( apply_filters( 'friends_show_author_edit', true, $args['friend_user'] ) ) : ?>
 	<li class="menu-item"><a href="<?php echo esc_url( self_admin_url( 'admin.php?page=edit-friend-rules&user=' . get_the_author_meta( 'ID' ) . '&post=' . get_the_ID() ) ); ?>" title="<?php esc_attr_e( 'Muffle posts like these', 'friends' ); ?>" class="friends-muffle-post">
 		<?php esc_html_e( 'Muffle posts like these', 'friends' ); ?>
 	</a></li>
+	<?php endif; ?>
 	<li class="menu-item">
 		<?php if ( 'trash' === get_post_status() ) : ?>
 			<a href="#" title="<?php /* phpcs:ignore WordPress.WP.I18n.MissingArgDomain */ esc_attr_e( 'Restore from Trash' ); ?>" data-trash-nonce="<?php echo esc_attr( wp_create_nonce( 'trash-post_' . get_the_ID() ) ); ?>" data-untrash-nonce="<?php echo esc_attr( wp_create_nonce( 'untrash-post_' . get_the_ID() ) ); ?>" data-id="<?php echo esc_attr( get_the_ID() ); ?>" class="friends-untrash-post">
