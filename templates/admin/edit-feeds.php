d4e1480294bbd4eb41bfa19fa958bc3761a224e3
Alex Kirk <akirk@users.noreply.github.com>
2024-09-02 11:34:02 +0200

    Update wording

--- templates/admin/edit-feeds.php
+++ templates/admin/edit-feeds.php
@@ -162,7 +162,7 @@ $has_last_log = false;
 				<td>
 					<label for="show_on_dashboard">
 						<input type="checkbox" name="show_on_dashboard" id="show_on_dashboard" value="1" <?php checked( $args['show_on_dashboard'] ); ?> />
-						<?php esc_html_e( 'Show posts on your wp-admin dashboard', 'friends' ); ?>
+						<?php esc_html_e( 'Show a widget for this friend on your wp-admin dashboard', 'friends' ); ?>
 					</label>
 				</td>
 			</tr>
