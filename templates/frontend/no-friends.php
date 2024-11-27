e707ea44b3f156587fdaa2b0c5c0b8662a665ce5
Alex Kirk <akirk@users.noreply.github.com>
2024-11-27 05:39:53 +0100

    Fix html appearing

--- templates/frontend/no-friends.php
+++ templates/frontend/no-friends.php
@@ -17,7 +17,7 @@ Friends\Friends::template_loader()->get_template_part(
 ?><p class="note">
 	<span>
 	<?php
-	esc_html_e( '<strong>Note:</strong> This box will go away as soon as you have added your first friend or subscription.', 'friends' );
+	echo wp_kses( __( '<strong>Note:</strong> This box will go away as soon as you have added your first friend or subscription.', 'friends' ), array( 'strong' => array() ) );
 	?>
 	</span>
 	<br>
