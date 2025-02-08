9894a773de0fb65ead7660e75c3ddad073c3564e
Alex Kirk <akirk@users.noreply.github.com>
2025-02-08 19:10:20 +0100

    revert change to accomodate non-updated ema

--- integrations/class-enable-mastodon-apps.php
+++ integrations/class-enable-mastodon-apps.php
@@ -17,12 +17,17 @@ class Enable_Mastodon_Apps {
 		add_filter( 'mastodon_api_get_posts_query_args', array( 'Friends\User', 'mastodon_api_get_posts_query_args' ) );
 		add_filter( 'mastodon_entity_relationship', array( 'Friends\User', 'mastodon_entity_relationship' ), 10, 2 );
 		add_action( 'mastodon_api_account_follow', array( get_called_class(), 'mastodon_api_account_follow' ), 10, 1 );
-		add_filter( 'mastodon_api_timelines_args', array( get_called_class(), 'mastodon_api_view_post_types' ) );
+		add_filter( 'mastodon_api_timelines_args', array( get_called_class(), 'mastodon_api_timelines_args' ) );
 		add_filter( 'mastodon_api_view_post_types', array( get_called_class(), 'mastodon_api_view_post_types' ) );
 	}
 
 	public static function mastodon_api_account_follow( $user_id ) {
 		return apply_filters( 'friends_create_and_follow', null, $user_id );
+	}
+
+	public static function mastodon_api_timelines_args( $args ) {
+		$args['post_type'][] = Friends::CPT;
+		return $args;
 	}
 
 	public static function mastodon_api_view_post_types( $view_post_types ) {
