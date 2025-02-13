129915e4051d89e68293e8fa8e0005111b5f8190
Alex Kirk <akirk@users.noreply.github.com>
2025-02-13 13:07:46 +0100

    Fix unfollowing in Enable Mastodon Apps (#471)

--- includes/class-user.php
+++ includes/class-user.php
@@ -1469,13 +1469,27 @@ class User extends \WP_User {
 	}
 
 	public static function mastodon_entity_relationship( $relationship, $user_id ) {
-		$user = Feed_Parser_ActivityPub::determine_mastodon_api_user( $user_id );
+		if ( ! class_exists( 'Friends\Feed_Parser_ActivityPub' ) ) {
+			if ( ! is_wp_error( $user_id ) ) {
+				$user = User::get_user_by_id( $user_id );
+				if ( ! $user ) {
+					$user = User::get_user_by_id( 'friends-virtual-user-' . $user_id );
+				}
+			}
+		} else {
+			$user = Feed_Parser_ActivityPub::determine_mastodon_api_user( $user_id );
+		}
 		if ( $user instanceof self ) {
 			if ( ! $relationship instanceof \Enable_Mastodon_Apps\Entity\Relationship ) {
 				$relationship = new \Enable_Mastodon_Apps\Entity\Relationship();
 			}
+			foreach ( $user->get_active_feeds() as $feed ) {
+				if ( Feed_Parser_ActivityPub::SLUG === $feed->get_parser() ) {
+					$relationship->following = true;
+					break;
+				}
+			}
 
-			$relationship->following = true;
 			if ( $user->has_cap( 'friend' ) ) {
 				$relationship->followed_by = true;
 			}
