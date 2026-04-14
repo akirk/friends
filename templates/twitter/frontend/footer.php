30e5305cc80e09dbd49dedffd54fe442ac25c6e1
Alex Kirk <akirk@users.noreply.github.com>
2026-04-14 04:14:29 +0200

    Add Twitter/X theme
    Add a new Twitter/X-inspired frontend theme, following the same pattern
    as the existing Mastodon theme with 3-column layout, compose box,
    keyboard shortcuts, and mention autocomplete.

---
+++ templates/twitter/frontend/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Twitter theme: page footer.
+ *
+ * @package Friends
+ */
+
+?>
+	</div><!-- .twitter-center-col -->
+
+	<!-- Right column: search + trending -->
+	<aside class="twitter-right-col" id="friends-sidebar">
+		<a class="twitter-sidebar-close" href="#close" aria-label="<?php esc_attr_e( 'Close sidebar', 'friends' ); ?>">&times;</a>
+		<form class="twitter-search-form form-autocomplete" action="<?php echo esc_url( home_url( '/friends/' ) ); ?>">
+			<div class="form-autocomplete-input twitter-search-wrap">
+				<?php
+				$_search = '';
+				if ( isset( $_GET['s'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+					$_search = sanitize_text_field( wp_unslash( $_GET['s'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+				}
+				?>
+				<input class="twitter-search-input master-search" type="text" name="s" placeholder="<?php /* phpcs:ignore WordPress.WP.I18n.MissingArgDomain */ esc_attr_e( 'Search' ); ?>" value="<?php echo esc_attr( $_search ); ?>" autocomplete="off" data-nonce="<?php echo esc_attr( wp_create_nonce( 'friends-autocomplete' ) ); ?>" id="master-search" />
+				<i class="form-icon"></i>
+			</div>
+			<ul class="menu" style="display: none"></ul>
+		</form>
+		<div class="twitter-right-customize">
+			<a href="<?php echo esc_url( add_query_arg( 'url', home_url( '/friends/' ), admin_url( 'customize.php?autofocus[section]=sidebar-widgets-friends-sidebar' ) ) ); ?>"><?php esc_html_e( 'customize sidebar', 'friends' ); ?></a>
+		</div>
+	</aside>
+
+	<a class="twitter-overlay" href="#close"></a>
+
+<?php wp_footer(); ?>
+</body>
+</html>
